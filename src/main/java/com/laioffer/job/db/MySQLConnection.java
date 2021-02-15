67935f96a6f69f2b65df0e07816d28e9259b0f4e
haochen1654 <hao.chen1654@gmail.com>
2021-02-14 20:14:07 -0800

    Added HistoryServlet

--- src/main/java/com/laioffer/job/db/MySQLConnection.java
+++ src/main/java/com/laioffer/job/db/MySQLConnection.java
@@ -5,7 +5,11 @@ import com.laioffer.job.entity.Item;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MySQLConnection {
     private Connection conn;
@@ -94,6 +98,83 @@ public class MySQLConnection {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public Set<String> getFavoriteItemIds(String userId) {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            return new HashSet<>();
+        }
+
+        Set<String> favoriteItems = new HashSet<>();
+
+        try {
+            String sql = "SELECT item_id FROM history WHERE user_id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                String itemId = rs.getString("item_id");
+                favoriteItems.add(itemId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return favoriteItems;
+    }
+
+    public Set<Item> getFavoriteItems(String userId) {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            return new HashSet<>();
+        }
+        Set<Item> favoriteItems = new HashSet<>();
+        Set<String> favoriteItemIds = getFavoriteItemIds(userId);
+
+        String sql = "SELECT * FROM items WHERE item_id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            for (String itemId : favoriteItemIds) {
+                statement.setString(1, itemId);
+                ResultSet rs = statement.executeQuery();
+                if (rs.next()) {
+                    favoriteItems.add(new Item(rs.getString("item_id")
+                            ,rs.getString("name")
+                            ,rs.getString("address")
+                            ,rs.getString("image_url")
+                            ,rs.getString("url")
+                            ,null
+                            , getKeywords(itemId)
+                            ,true));
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return favoriteItems;
+    }
+
+    public Set<String> getKeywords(String itemId) {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            return Collections.emptySet();
+        }
+        Set<String> keywords = new HashSet<>();
+        String sql = "SELECT keyword from keywords WHERE item_id = ? ";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, itemId);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                String keyword = rs.getString("keyword");
+                keywords.add(keyword);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return keywords;
     }
 
 }
--- src/main/java/com/laioffer/job/servlet/SearchServlet.java
+++ src/main/java/com/laioffer/job/servlet/SearchServlet.java
@@ -1,6 +1,7 @@
 package com.laioffer.job.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.db.MySQLConnection;
 import com.laioffer.job.entity.Item;
 import external.GitHubClient;
 
@@ -9,20 +10,32 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 @WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String userId = request.getParameter("user_id");
         double lat = Double.parseDouble(request.getParameter("lat"));
         double lon = Double.parseDouble(request.getParameter("lon"));
 
+        MySQLConnection connection = new MySQLConnection();
+        Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+        connection.close();
+
         GitHubClient client = new GitHubClient();
         List<Item> items = client.search(lat, lon, null);
-        response.setContentType("application/json");
+
+        for (Item item : items) {
+            item.setFavorite(favoritedItemIds.contains(item.getId()));
+        }
+
         ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getWriter(), items);
+        response.setContentType("application/json");
+        response.getWriter().print(mapper.writeValueAsString(items));
+
     }
 
     @Override
